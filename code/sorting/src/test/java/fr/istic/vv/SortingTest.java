87b53b99a808b49af07b87b72abd3b5d15dbb9d9
LayeKaba <50553914+LayeKaba@users.noreply.github.com>
2022-03-17 17:04:00 +0100

    Update SortingTest.java

--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -6,4 +6,15 @@ public class SortingTest {
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
+    
+    
+     @Provide
+    Arbitrary<Integer[]> ArrProvider() {
+        Arbitrary<Integer> integerArbitrary = Arbitraries.integers().between(1, 100);
+        return integerArbitrary
+                .array(Integer[].class).ofMinSize(0).ofMaxSize(30)
+                .map(anArray -> {
+                    return anArray;
+                });
+    }
 }
