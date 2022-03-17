e521e8b4839433f9d081f95723ae5c4e3f6165d3
LayeKaba <50553914+LayeKaba@users.noreply.github.com>
2022-03-17 17:18:10 +0100

    Update SortingTest.java

--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -7,7 +7,26 @@ public class SortingTest {
         return Math.abs(anInteger) >= 0;
     }
     
-    
+    @Property
+    boolean arrayIsSorted(@ForAll Integer[] anArray) {
+
+        Integer[] bubbleCopy,quickCopy,mergeCopy;
+        triBulle = anArray.clone();
+        triRapide = anArray.clone();
+        triFusion = anArray.clone();
+        Arrays.sort(anArray);
+
+        Sorting.bubblesort(triBulle, Integer::compareTo);
+        boolean triBulleResultat = Arrays.equals(triBulle, anArray);
+
+        Sorting.quicksort(triRapide, Integer::compareTo);
+        boolean triRapideResultat = Arrays.equals(triRapide, anArray);
+
+        Sorting.mergesort(triFusion, Integer::compareTo);
+        boolean triFusionResultat = Arrays.equals(triFusion, anArray);
+
+        return triBulleResultat && triRapideResultat && triFusionResultat;
+    }
      @Provide
     Arbitrary<Integer[]> ArrProvider() {
         Arbitrary<Integer> integerArbitrary = Arbitraries.integers().between(1, 100);
