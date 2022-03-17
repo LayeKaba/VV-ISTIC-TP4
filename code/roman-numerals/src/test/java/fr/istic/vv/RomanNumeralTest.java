f2d11dae378623bbdece7bb13cbef996ed4ac20e
Elodie <91661119+eloLG@users.noreply.github.com>
2022-03-17 17:41:28 +0100

    Update RomanNumeralTest.java

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -2,8 +2,28 @@ package fr.istic.vv;
 import net.jqwik.api.*;
 
 public class RomanNumeralTest {
+    
     @Property
+    @Disabled
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    @Property
+    boolean tousLesNombresRomainsOntUneLongueurPositive(@ForAll String string){
+        if (RomanNumeraUtils.isValidRomanNumeral(string)){
+            return string.length() > 0;
+        }else{
+            return !RomanNumeraUtils.isValidRomanNumeral(string);
+        }
+    }
+
+    @Property
+    boolean symbolesNombresRomains(@ForAll String string) {
+        if (RomanNumeraUtils.isValidRomanNumeral(string)){
+            return string.matches("^[MDCLXVI]+$");
+        }else{
+            return !RomanNumeraUtils.isValidRomanNumeral(string);
+        }
     }
 }
